8fcdb8b62e961c0c0d46c8a592ab79996a71550a
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-09-08 15:38:09 +0200

    cookieconsent bar
    herstel cookieconsent bar door deze van layout naar footer te verplaatsen. Hierdoor moet hij meescalen met mobiel

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { MDXProvider } from "@mdx-js/react"
 import TopMenu from './topmenu'
 import Footer from'./footer'
-import CookieConsentBar from "./cookieconsentbar";
 
 export default function Layout  ({ children  }) {
   return (   
@@ -12,7 +11,7 @@ export default function Layout  ({ children  }) {
         }} >{children}</MDXProvider>
  </main>
  <Footer/>
- <CookieConsentBar/>
+
  </div>
   )
 }
