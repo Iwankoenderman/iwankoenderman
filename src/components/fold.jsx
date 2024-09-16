c48d3e804c5564de2c4a50a5339de24f136047a1
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-09-16 23:27:36 +0200

    Update fold.jsx
    krachtiger en netjes

--- src/components/fold.jsx
+++ src/components/fold.jsx
@@ -10,9 +10,9 @@ export default function Fold() {
 <>
                        <div className='mainblock__hoofdrow'>
                             <div className='mainblock__hoofdrow--fold'>
-                                 {intl.locale === "nl" ? (<p>Maak een afspraak en start:</p>) : (<p>Make an appointment and start: </p>)}
-                                 {intl.locale === "nl" ? (<p>● helder verwoorde boodschap</p>) : (<p>● a clear message;</p>)} 
-                                 {intl.locale === "nl" ? (<p>● jouw social media strategie </p>) : (<p>● your social media strategy</p>)}
+                                 {intl.locale === "nl" ? (<p>Plan een afspraak, start met:</p>) : (<p>Make an appointment, start with: </p>)}
+                                 {intl.locale === "nl" ? (<p>● je verwoorde niche</p>) : (<p>● a clear message;</p>)} 
+                                 {intl.locale === "nl" ? (<p>● je social media posts </p>) : (<p>● your social posts</p>)}
                                  {intl.locale === "nl" ? (<p>● claim je expert status</p>) : (<p>● claim your expert status;</p>)}                                
                            </div>
                        </div>   
