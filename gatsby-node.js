ec5aa87d111aa8f767efc3777d98ec2ea67339b4
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-09-24 20:19:41 +0200

    Update gatsby-node.js
    redirect google analytics weg

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,12 @@ const gtmTrackingId = process.env.GATSBY_GOOGLE_TAG_MANAGER_TRACKING_ID;
 
 exports.onPreBuild = async ({ actions: { createRedirect } })=> {
   await copyLibFiles(path.join(__dirname, "static", "~partytown"));
+
+  createRedirect({
+    fromPath:  `/__third-party-proxy?url=https://www.googletagmanager.com/gtag/js?id=${gtmTrackingId}`,
+    toPath: `https://www.googletagmanager.com/gtag/js?id=${gtmTrackingId}`,
+    statusCode: 200,
+  })
 }
 
 
