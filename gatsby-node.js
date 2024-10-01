6665569c09a9e25daa84796cd0476aa365ab19d3
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-10-01 20:50:19 +0200

    Update gatsby-node.js
    verwijdering redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,8 @@ const path = require("path");
 const { copyLibFiles } = require('@builder.io/partytown/utils');
 const gtmTrackingId = process.env.GATSBY_GOOGLE_TAG_MANAGER_TRACKING_ID;
 
-exports.onPreBuild = async ({ actions: { createRedirect } })=> {
+exports.onPreBuild = async ({ actions: { } })=> {
   await copyLibFiles(path.join(__dirname, "static", "~partytown"));
-
-  createRedirect({
-    fromPath:  `/__third-party-proxy?url=https://www.googletagmanager.com/gtag/js?id=${gtmTrackingId}`,
-    toPath: `https://www.googletagmanager.com/gtag/js?id=${gtmTrackingId}`,
-    statusCode: 200,
-  })
-
-  createRedirect({
-    fromPath:  `/__third-party-proxy?url=https://www.google-analytics.com/analytics.js`,
-    toPath: `https://www.google-analytics.com/analytics.js`,
-    statusCode: 200,
-  })
 }
 
 
