9017001df5ccf113963953861cbdcd1a8f100cd3
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-10-04 00:10:59 +0200

    Update gatsby-ssr.js
    ='${gaTrackingId}'; wordt id=${gaTrackingId};

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -34,7 +34,7 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyC
     dangerouslySetInnerHTML={{
       __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src='/gtag/js?id='${gaTrackingId}';
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src='/gtag/js?id=${gaTrackingId};
             m.parentNode.insertBefore(a,m)
             })(window,document,'script','${ORIGIN}/gtag/js?id='${gaTrackingId}','gtag');
   
