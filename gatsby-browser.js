ce1f6a5cd90713a0ea649f473022064047183b96
Iwankoenderman <67146641+Iwankoenderman@users.noreply.github.com>
2024-10-06 16:08:30 +0200

    update gatsby-browser.js en gatsby-ssr.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -55,9 +55,16 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyC
     />,   
     <script 
     key="linkedin-insight" 
+    strategy="off-main-thread"
     type="text/javascript"
-     src="https://snap.licdn.com/li.lms-analytics/insight.min.js" async>
-    </script>,
+    dangerouslySetInnerHTML={{
+      __html:` (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+         window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script"); b.type = "text/javascript";b.async = true; 
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})
+        (window.lintrk);`
+    }}
+    />,
 
    ]),
    setPreBodyComponents([
@@ -70,15 +77,18 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyC
                 `,
       }}
     />,
-    <noscript key="linkedin-noscript">
+    <noscript key="linkedin-noscript"
+    dangerouslySetInnerHTML={{
+        __html: `
     <img
       height="1"
       width="1"
       style={{ display: 'none' }}
       alt=""
-      src={`https://px.ads.linkedin.com/collect/?pid=${linkedinTrackingId}&fmt=gif`}
-    />
-  </noscript>,
+      src="https://px.ads.linkedin.com/collect/?pid=${linkedinTrackingId}&fmt=gif">
+       `,
+      }}
+    />,
   ]);
 }
 
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -58,10 +58,16 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyC
 
     <script 
     key="linkedin-insight" 
+    strategy="off-main-thread"
     type="text/javascript"
-     src="https://snap.licdn.com/li.lms-analytics/insight.min.js" async>
-    </script>,
-  
+    dangerouslySetInnerHTML={{
+      __html:` (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+         window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script"); b.type = "text/javascript";b.async = true; 
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})
+        (window.lintrk);`
+    }}
+    />,
    ]),
    setPreBodyComponents([
     <noscript
@@ -74,15 +80,19 @@ export const onRenderBody = ({ setHeadComponents, setHtmlAttributes, setPreBodyC
       }}
     />,
 
-    <noscript key="linkedin-noscript">
+    <noscript key="linkedin-noscript"
+    dangerouslySetInnerHTML={{
+        __html: `
     <img
       height="1"
       width="1"
       style={{ display: 'none' }}
       alt=""
-      src={`https://px.ads.linkedin.com/collect/?pid=${linkedinTrackingId}&fmt=gif`}
+      src="https://px.ads.linkedin.com/collect/?pid=${linkedinTrackingId}&fmt=gif">
+       `,
+      }}
     />
-  </noscript>,
+  ,
   ]);
 }
 
